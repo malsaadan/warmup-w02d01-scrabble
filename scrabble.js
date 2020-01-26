bb52f8c57bb6394f3c341f5185cca9dfa33d0a2e
malsaadan <mmalsaadan@gmail.com>
2020-01-26 09:45:43 +0300

    warmup solution

---
+++ scrabble.js
@@ -0,0 +1,41 @@
+const scrabbleScore = function(word) {
+    let scoreVal = 0;
+    const letterVal = {
+        A: 1,
+        E: 1,
+        I: 1,
+        O: 1,
+        U: 1,
+        L: 1,
+        N: 1,
+        R: 1,
+        S: 1,
+        T: 1,
+        D: 3,
+        G: 2,
+        B: 3,
+        C: 3,
+        M: 3,
+        P: 3,
+        F: 4,
+        H: 4,
+        V: 4,
+        W: 4,
+        Y: 4,
+        K: 5,
+        J: 8,
+        X: 8,   
+        Q: 10,
+        Z: 10,
+    };
+
+    let score = 0, inChar = "", val = 0;
+    for(let i = 0; i < word.length; i++){
+        inChar = word.charAt(i);
+        val = letterVal[inChar.toUpperCase()];
+        console.log(val + ' point for ' + inChar)
+        score += val;
+    }
+
+    return score;
+}
